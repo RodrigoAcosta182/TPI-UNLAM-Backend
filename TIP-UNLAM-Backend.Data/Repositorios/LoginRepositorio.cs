db58ab821bdfbfd137608820bb5bf8648b72d922
“Emiliano <emiortiz1992@gmail.com>
2022-09-03 15:52:31 -0300

    subo cositas

---
+++ TIP-UNLAM-Backend.Data/Repositorios/LoginRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIP_UNLAM_Backend.Data.Repositorios
+{
+    internal class LoginRepositorio
+    {
+    }
+}
---
+++ TPI-UNLAM-Backend.Web/Controllers/v1/JuegoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TPI_UNLAM_Backend.Servicios.Interfaces;
+
+namespace TPI_UNLAM_Backend.Controllers.v1
+{
+    public class JuegoController : Controller
+    {
+
+        private readonly IJuegoServicio _juegoServicio;
+
+        public JuegoController(IJuegoServicio juegoServicio)
+        {
+            _juegoServicio = juegoServicio;
+        }
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
