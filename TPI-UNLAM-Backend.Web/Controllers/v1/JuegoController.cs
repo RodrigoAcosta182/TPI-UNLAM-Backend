535c0f6aa2662d6449993555b26f9681ae7e9f71
“Emiliano <emiortiz1992@gmail.com>
2022-09-04 20:37:20 -0300

    Subo controller de juego

--- TPI-UNLAM-Backend.Web/Controllers/v1/JuegoController.cs
+++ TPI-UNLAM-Backend.Web/Controllers/v1/JuegoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TIP_UNLAM_Backend.Data.EF;
 using TPI_UNLAM_Backend.Servicios.Interfaces;
 
 namespace TPI_UNLAM_Backend.Controllers.v1
@@ -17,5 +19,12 @@ namespace TPI_UNLAM_Backend.Controllers.v1
         {
             return View();
         }
+
+        [HttpGet("api/v1/auth")]
+        public ActionResult<List<Juego>> autenticarUsuario()
+        {
+            return _juegoServicio.getAllJuegos();
+        }
+
     }
 }
