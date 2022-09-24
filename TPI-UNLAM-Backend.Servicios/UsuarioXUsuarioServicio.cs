6d3d78fc9f08799c0f6a18949024e91ba6f2292f
“Emiliano <emiortiz1992@gmail.com>
2022-09-24 00:58:07 -0300

    mis pacientes

---
+++ TPI-UNLAM-Backend.Servicios/UsuarioXUsuarioServicio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TIP_UNLAM_Backend.Data.EF;
+using TIP_UNLAM_Backend.Data.Repositorios.Interfaces;
+using TPI_UNLAM_Backend.Servicios.Interfaces;
+
+namespace TPI_UNLAM_Backend.Servicios
+{
+    public class UsuarioXUsuarioServicio : IUsuarioXUsuarioServicio
+    {
+        private IUsuarioXUsuarioRepositorio _userXuser;
+
+        public UsuarioXUsuarioServicio(IUsuarioXUsuarioRepositorio userXuser)
+        {
+            _userXuser = userXuser;
+        }
+
+        public List<UsuarioXusuario> getPacienteXProfesional(int UsuarioLogeadoId)
+        {
+            return _userXuser.getPacienteXProfesional(UsuarioLogeadoId).ToList();
+        }
+    }
+}
