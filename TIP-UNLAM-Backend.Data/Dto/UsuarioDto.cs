e0f7172efc3af17d5f8f28bcf2e90fba43b733fb
julioRodrigoAcosta <jracosta1991@gmail.com>
2022-09-25 14:30:37 -0300

    cambio UsuarioDto

--- TIP-UNLAM-Backend.Data/Dto/UsuarioDto.cs
+++ TIP-UNLAM-Backend.Data/Dto/UsuarioDto.cs
@@ -11,6 +11,6 @@ namespace TIP_UNLAM_Backend.Data.Dto
     public class UsuarioDto
     {
         public Usuario usuario { get; set; }
-        public UserTokenDto token { get; set; }
+        public string token { get; set; }
     }
 }
