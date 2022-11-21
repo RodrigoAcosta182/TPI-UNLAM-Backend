26ab3a210155230719c0f3489a6946859446afcd
julioRodrigoAcosta <jracosta1991@gmail.com>
2022-11-21 15:58:36 -0300

    cambios hub message

--- Hubs/MessageHub.cs
+++ Hubs/MessageHub.cs
@@ -1,29 +1,52 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Grandin.Web.EF;
+using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
+using TIP_UNLAM_Backend.Data.Repositorios.Interfaces;
+using TPI_UNLAM_Backend.Servicios.Interfaces;
 
 namespace TPI_UNLAM_Backend.Hubs
 {
     public class MessageHub : Hub
     {
-
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage",user, message);
-            
-        }
         
-        public async Task AgregarAGrupo(string grupo)
+        //private IAppSharedFunction _appSharedFunction;
+        //private IUsuarioRepositorio _userRepo;
+        //private string _emailTemp;
+        //public MessageHub( IAppSharedFunction appSharedFunction, IUsuarioRepositorio userRepom, string email )
+        //{
+           
+        //    _appSharedFunction = appSharedFunction;
+        //    _userRepo = userRepom;
+        //    _emailTemp = email;
+        //}
+        public override Task OnConnectedAsync()
         {
-            await Groups.AddToGroupAsync(GetConnectionId(), grupo);
+            return base.OnConnectedAsync();
+        }
+
+        public  override Task OnDisconnectedAsync(Exception exception)
+        {
+            //QuitarDeGrupo(_emailTemp);
+            return base.OnDisconnectedAsync(exception);
+        }
+        public async Task AgregarAGrupo(string email)
+        {
+
+            //_emailTemp = email;
+            //Usuario usuario = _userRepo.getUsuarioByEmail(email);
+            await Groups.AddToGroupAsync(GetConnectionId(), email);
+        } 
+        
+        public void QuitarDeGrupo(string email)
+        {
+            //Usuario usuario = _userRepo.getUsuarioByEmail(email);
+            Groups.RemoveFromGroupAsync(GetConnectionId(), email);
         }
 
         public string GetConnectionId()
         {
             return Context.ConnectionId;
         }
-        //public async Task SendToUser(string user, string receiverConnectionId, string message)
-        //{
-        //    await Clients.Client(receiverConnectionId).SendAsync("ReceiveMessage",user, message);
-        //}
     }
 }
